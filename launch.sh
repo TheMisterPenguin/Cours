f859b873b4758e8b1b61e4ffc7ff4bc15b1a2709
Ange Despert <>
2021-04-11 20:50:37 +0000

    Ajout des commentaires / Comments added

--- launch.sh
+++ launch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-loc=`find -name file.start | cut -d/ -f1-4 `
-file=`find -name file.start -print`
-nom=`cat $file | head -n1`
-nbligne=`cat $file | wc -l`
-nbligne=`expr $nbligne - 1`
-cd $loc; gcc $nom.c ;mv a.out $nom.prog; ./$nom.prog
+loc=`find -name file.start | cut -d/ -f1-4 ` # Localisation de file.start
+file=`find -name file.start -print` # Sauvegarde du chemin complet vers le fichier
+nom=`cat $file | head -n1` # Lecture de la 1ère ligne (Nom du programme)
+#nbligne=`cat $file | wc -l` # test
+#nbligne=`expr $nbligne - 1` # test
+cd $loc; gcc $nom.c ;mv a.out $nom.prog; ./$nom.prog # Compilation et exécution du programme
+# A ajouter :
+#   - arguments pris en compte sur les lignes qui suivent
