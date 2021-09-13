552523f76439bf0695e11a28d1d4a30aa1b5afd8
TheMisterPenguin <74404918+TheMisterPenguin@users.noreply.github.com>
2021-09-13 19:17:36 +0200

    .

---
+++ Algo2/TD/TD1/2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void compter(FILE *fichier, int *nb_caract, int *nb_lignes){
+    //Fonction qui compte le nombre de caractères et de lignes du fichier passé en paramètre
+    char lecture;
+
+    fscanf(fichier,"%c", lecture);
+    while(! feof(fichier)){
+        switch (lecture)
+        {
+        case ' ' :
+            break;
+        case '\n' : (*nb_lignes)++;
+            break;
+        default :  (*nb_caract)++;
+            break;
+        }
+        fscanf(fichier,"%c", lecture);
+    }
+
+}
+
+int main(int argc, char** argv){
+    FILE* fichier = NULL;
+    int nb_caract;
+    int nb_lignes;
+
+    if(argc < 2){
+        printf("Il manque un argument. Il en faut au moins 1.");
+        return 1;
+    }
+
+    fichier = fopen(argv[1], "r");
+    if(fichier == NULL){
+        printf("Le fichier \"%s\" n'existe pas ! \n", argv[1]);
+        return 2;
+    }
+    compter(fichier, &nb_caract, &nb_lignes);
+    printf("IL y a %i caractères et %i lignes dans le fichier \"%s\"\n", nb_caract, nb_lignes, argv[1]);
+    fclose(fichier);
+
+    return 0;
+}
